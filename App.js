f86233d8734c6d6c2cc4de52d1aa1c9c928b6709
Panagiwtis Georgiadis <panagiwtis.geor@gmail.com>
2017-06-23 22:12:46 +0100

    Commiting the final Chat App that was build in the meetup.

--- App.js
+++ App.js
@@ -1,13 +1,83 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, KeyboardAvoidingView, StatusBar, FlatList, Image} from 'react-native';
+
+import ReversedFlatList from 'react-native-reversed-flat-list';
+import {send, subscribe} from 'react-native-training-chat-server';
+
+import Header from "./Header";
+
+const NAME = '@PanagiotisGeorgiadis';
+const CHANNEL = 'Reactivate';
+const AVATAR = 'https://pbs.twimg.com/profile_images/869307058931281921/xuQAx3pT.jpg';
 
 export default class App extends React.Component {
-  render() {
+
+  constructor() {
+    super();
+    this.state = {
+      message: '',
+      messagesArray: []
+    }
+  }
+
+  componentWillMount() {
+    subscribe(CHANNEL, messagesArray => {
+      this.setState({messagesArray});
+    });
+  }
+
+  sendMessage = async () => {
+    // read message from component state
+    const message = this.state.message;
+
+    // send message to our channel, with sender name
+    await send({
+      channel: CHANNEL,
+      sender: NAME,
+      avatar: AVATAR,
+      message,
+    });
+
+    // set the component state (clears text input)
+    this.setState({
+      message: '',
+    });
+  };
+
+  renderItem({item}) {
+
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View style = { styles.row } >
+        <Image style = { styles.avatar } source={{ uri: item.avatar }} />
+        <View style = { styles.rowText }>
+          <Text style = { styles.sender }>
+            { item.sender }
+          </Text>
+          <Text style = { styles.message }>
+            { item.message }
+          </Text>
+        </View>
+      </View>
+    );
+  }
+
+  render() {
+
+    return (
+      <View style={ styles.container } >
+        <Header titleText = { CHANNEL } />
+        <ReversedFlatList data = { this.state.messagesArray } renderItem = { this.renderItem } />
+        <KeyboardAvoidingView behavior = "padding">
+          <View style = { styles.footer }>
+            <TextInput value = { this.state.message } style = { styles.input } underlineColorAndroid = "transparent"
+              placeholder = "Type something nice" onChangeText = { text => this.setState({ message: text }) } />
+            <TouchableOpacity onPress = { this.sendMessage } >
+              <Text style = { styles.send }>
+                Send
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </KeyboardAvoidingView>
       </View>
     );
   }
@@ -17,7 +87,43 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  row: {
+    flexDirection: 'row',
+    padding: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: '#eee',
+  },
+  avatar: {
+    borderRadius: 20,
+    width: 40,
+    height: 40,
+    marginRight: 10,
+  },
+  rowText: {
+    flex: 1,
+  },
+  message: {
+    fontSize: 16,
+  },
+  sender: {
+    fontWeight: 'bold',
+    paddingRight: 10,
+  },
+  footer: {
+    flexDirection: 'row',
+    backgroundColor: '#eee',
+  },
+  input: {
+    paddingHorizontal: 20,
+    fontSize: 18,
+    flex: 1,
+  },
+  send: {
+    alignSelf: 'center',
+    color: 'lightseagreen',
+    fontSize: 16,
+    fontWeight: 'bold',
+    padding: 20,
   },
 });
